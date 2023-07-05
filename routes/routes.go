658c7e995554f40b1526380779c3aafc966eb55f
fededomm <fededomm@outlook.it>
2023-07-05 16:38:58 +0200

    fix

--- routes/routes.go
+++ routes/routes.go
@@ -20,6 +20,86 @@ type Routes struct {
 	DB *mongo.Client
 }
 
+func getNextSeq(name string, c *gin.Context, r *Routes) (*models.Counter, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	counterCollection := db.GetCollection(r.DB, "counter")
+	filter := bson.M{"_id": name}
+
+	// EVITARE LA CONCURRENCY SENZA MAI SPECIFICARE IL VALORE
+	replace := bson.M{"$inc": bson.M{"seq": 1}}
+
+	upsert := true
+	before := options.Before
+	opt := options.FindOneAndUpdateOptions{
+		ReturnDocument: &before,
+		Upsert:         &upsert,
+	}
+
+	result := counterCollection.FindOneAndUpdate(
+		ctx, filter, replace, &opt,
+	)
+
+	if result.Err() != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"ERROR": result.Err().Error()})
+		return nil, result.Err()
+	} else {
+		var counter models.Counter
+		counter.Id = name
+		res := result.Decode(&counter)
+		return &counter, res
+	}
+}
+
+// CreateRecord
+//
+//	@Summary		Post one Ordine
+//	@Description	Crea un record nella Collection Ordini
+//	@Tags			Ordini
+//	@Accept			json
+//	@Produce		json
+//	@Param			json	body	string	true	"Inserisci un Ordine"
+//	@Success		200
+//	@Failure		400
+//	@Failure		404
+//	@Failure		500
+//	@Router			/gest [post]
+func (r *Routes) PostOrdini(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ordini := new(models.Ordini)
+	defer cancel()
+	if err := c.ShouldBindJSON(&ordini); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		log.Println(err.Error())
+		return
+	}
+	numeroOrdine, rerr := getNextSeq("ordineId", c, r)
+	if rerr != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"ERROR": rerr.Error()})
+	}
+
+	postPayload := models.Ordini{
+		NumeroOrdine: numeroOrdine.Seq,
+		Oggetto:      ordini.Oggetto,
+		Data:         primitive.NewDateTimeFromTime(time.Now()),
+		Destinatario: ordini.Destinatario,
+		Mittente:     ordini.Mittente,
+		Prezzo:       ordini.Prezzo,
+	}
+
+	ordiniCollection := db.GetCollection(r.DB, "Ordini")
+
+	result, err := ordiniCollection.InsertOne(ctx, postPayload)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+
+	}
+	c.JSON(http.StatusCreated, gin.H{"message": "Posted successfully", "data": result, "payload": postPayload})
+}
+
+
 // GetAllOrdini
 //
 //	@Summary		List All Ordini
@@ -89,52 +169,7 @@ func (r *Routes) GetSingleOrdine(c *gin.Context) {
 
 }
 
-// CreateRecord
-//
-//	@Summary		Post one Ordine
-//	@Description	Crea un record nella Collection Ordini
-//	@Tags			Ordini
-//	@Accept			json
-//	@Produce		json
-//	@Param			json	body	string	true	"Inserisci un Ordine"
-//	@Success		200
-//	@Failure		400
-//	@Failure		404
-//	@Failure		500
-//	@Router			/gest [post]
-func (r *Routes) PostOrdini(c *gin.Context) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	ordini := new(models.Ordini)
-	defer cancel()
-	if err := c.ShouldBindJSON(&ordini); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
-		log.Println(err.Error())
-		return
-	}
-	numeroOrdine, rerr := r.getNextSeq("ordineId", c)
-	if rerr != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"ERROR": rerr.Error()})
-	}
 
-	postPayload := models.Ordini{
-		NumeroOrdine: numeroOrdine.Seq,
-		Oggetto:      ordini.Oggetto,
-		Data:         primitive.NewDateTimeFromTime(time.Now()),
-		Destinatario: ordini.Destinatario,
-		Mittente:     ordini.Mittente,
-		Prezzo:       ordini.Prezzo,
-	}
-
-	ordiniCollection := db.GetCollection(r.DB, "Ordini")
-
-	result, err := ordiniCollection.InsertOne(ctx, postPayload)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-		return
-
-	}
-	c.JSON(http.StatusCreated, gin.H{"message": "Posted successfully", "data": result, "payload": postPayload})
-}
 
 // Update Ordini
 //
@@ -228,34 +263,5 @@ func (r *Routes) DeleteOrdine(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"Messaggio": "Ordine eliminato con successo"})
 }
 
-func (r *Routes) getNextSeq(name string, c *gin.Context) (*models.Counter, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
 
-	counterCollection := db.GetCollection(r.DB, "counter")
-	filter := bson.M{"_id": name}
 
-	// EVITARE LA CONCURRENCY SENZA MAI SPECIFICARE IL VALORE
-	replace := bson.M{"$inc": bson.M{"seq": 1}}
-
-	upsert := true
-	before := options.Before
-	opt := options.FindOneAndUpdateOptions{
-		ReturnDocument: &before,
-		Upsert:         &upsert,
-	}
-
-	result := counterCollection.FindOneAndUpdate(
-		ctx, filter, replace, &opt,
-	)
-
-	if result.Err() != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"ERROR": result.Err().Error()})
-		return nil, result.Err()
-	} else {
-		var counter models.Counter
-		counter.Id = name
-		res := result.Decode(&counter)
-		return &counter, res
-	}
-}
